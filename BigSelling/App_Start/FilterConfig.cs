d8a0d653e6ffcd43cfaad7c744b20137d87aac60
gerardo.aquino25 <gerardo.aquino25@gmail.com>
2016-07-25 03:36:00 -0300

    Start commit.

---
+++ BigSelling/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace BigSelling
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ BigSelling/Scripts/BigSelling.js
@@ -0,0 +1,19 @@
+﻿var BigSelling = angular.module('BigSelling', ['ngRoute']);
+
+BigSelling.controller('LandingPageController', LandingPageController);
+
+var configFunction = function ($routeProvider) {
+    $routeProvider.
+        when('/routeOne', {
+            templateUrl: 'routesDemo/one'
+        })
+        .when('/routeTwo/:donuts', {
+            templateUrl: function (params) { return '/routesDemo/two?donuts=' + params.donuts; }
+        })
+        .when('/routeThree', {
+            templateUrl: 'routesDemo/three'
+        });
+}
+configFunction.$inject = ['$routeProvider'];
+
+BigSelling.config(configFunction);
